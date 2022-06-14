508a6f0e6005fd8845557d96d514afdc1b2c1126
Alaina Shumate <alainashumate@gmail.com>
2022-06-14 13:46:25 -0400

    Update setup.py
    updated

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import setuptools
 
 setuptools.setup(
     name="LiftoffTools",
-    version="0.2.0",
+    version="0.1.0",
     author="Alaina Shumate",
     author_email="ashumat2@jhmi.edu",
     description="A tool for comparing annotations across genome assemblies",
     url="https://github.com/ashumate/Planet",
-    install_requires=[],
+    install_requires=['nltk==3.6.7','numpy==1.21.1', 'parasail==1.2.4', 'gffutils==0.10.1', 'pyfaidx==0.5.8', 'biopython==1.76'],
     python_requires='>=3.6',
     packages=['liftofftools'],
     entry_points={'console_scripts': ['liftofftools = liftofftools.liftofftools:main'], },
