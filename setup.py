60c67d9ac23e03b764f40770defb6e569c925ce1
Alaina Shumate <alainashumate@gmail.com>
2022-06-14 16:48:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setuptools.setup(
     url="https://github.com/ashumate/Planet",
     install_requires=['nltk==3.6.7','numpy==1.21.1', 'parasail==1.2.4', 'gffutils==0.10.1', 'pyfaidx==0.5.8', 'biopython==1.76'],
     python_requires='>=3.6',
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
     entry_points={'console_scripts': ['liftofftools = liftofftools.liftofftools:main'], },
 )
