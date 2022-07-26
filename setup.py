e94842262bef05ae497ec325c1d60112e5b88800
Shumate <alaina.shumate@fdt010am-580494.scitech.dhs>
2022-07-26 13:43:07 -0400

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="LiftoffTools",
-    version="0.2.0",
+    version="0.3.0",
     author="Alaina Shumate",
     author_email="ashumat2@jhmi.edu",
     description="A tool for comparing annotations across genome assemblies",
